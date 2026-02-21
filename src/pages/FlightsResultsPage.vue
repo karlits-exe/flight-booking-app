28fe136463fe9c80ac6d709804cdfbae17d2d1ae
lorddennisjoplin <lorddennisjoplin@gmail.com>
2026-02-21 17:01:26 +0800

    Fix flight results page

--- src/pages/FlightsResultsPage.vue
+++ src/pages/FlightsResultsPage.vue
@@ -151,7 +151,9 @@
 
     	<div v-else>
     		<div v-if="flights.length" class="px-md-2 px-4">
-          <h1 class="h4 mb-0">Select Flight<span v-if="!searchData.oneWay">s</span>
+          <h1 class="h4 mb-0">Select
+            <span v-if="searchData.oneWay">one-way flight</span>
+            <span v-else>round-trip flights</span>
           </h1>
 
           <hr style="height: 3px;" class="bg-dark border-0 my-5" />
