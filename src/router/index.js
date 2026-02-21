34103738c888c7e3b966a880dab2b5ed20a962c6
lorddennisjoplin <lorddennisjoplin@gmail.com>
2026-02-21 21:13:27 +0800

    Dynamically change title

--- src/router/index.js
+++ src/router/index.js
@@ -5,15 +5,19 @@ import Homepage from "../pages/HomePage.vue"
 import FlightsResults from "../pages/FlightsResultsPage.vue"
 
 const routes = [
-  { path: "/", component: Homepage },
-  { path: "/login", component: LogIn },
-  { path: "/signup", component: SignUp },
-  { path: "/results", component: FlightsResults }
+  { path: "/", component: Homepage, meta: { title: 'Home | World Class' } },
+  { path: "/login", component: LogIn, meta: { title: 'Log In | World Class' } },
+  { path: "/signup", component: SignUp, meta: { title: 'Sign Up | World Class' } },
+  { path: "/results", component: FlightsResults, meta: { title: 'Find Flights | World Class' } }
 ]
 
 const router = createRouter({
   history: createWebHistory(),
   routes
 })
+
+router.afterEach((to) => {
+  document.title = to.meta.title || 'World Class';
+});
 
 export default router
